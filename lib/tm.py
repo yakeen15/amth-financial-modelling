183aae854fc4dfd4881e85b165f3eeb36bd29a76
yakeen15 <shajiratulyakeen@gmail.com>
2024-02-17 08:50:58 +0600

    fixed trinomial method

--- lib/tm.py
+++ lib/tm.py
@@ -13,18 +13,19 @@ def trinomial_tree_option_price(S, K, T, r, sigma, n, option_type='call'):
 
     stock_prices = np.zeros((2*n+1, n+1))
     option_values = np.zeros((2*n+1, n+1))
-
-    for j in range(n+1):
-        for i in range(-j, j+1, 2):
-            stock_prices[i+n, j] = S * (u ** (j - abs(i)))
-
+    stock_prices[n,0] = S
+    for j in range(1,n+1):
+        stock_prices[n-j,j] = S*u**j
+        for i in range(n-j+1,n+j+1):
+            stock_prices[i,j] = stock_prices[i-1,j]/u
+    print(stock_prices)
     if option_type == 'call':
         option_values[:, -1] = np.maximum(0, stock_prices[:, -1] - K)
     elif option_type == 'put':
         option_values[:, -1] = np.maximum(0, K - stock_prices[:, -1])
 
     for j in range(n-1, -1, -1):
-        for i in range(-j, j+1, 2):
+        for i in range(-j, j+1):
             option_values[i+n, j] = np.exp(-r * delta_t) * (p_u * option_values[i+n+1, j+1] +
                                                             p_d * option_values[i+n-1, j+1] +
                                                             p_m * option_values[i+n, j+1])
--- lib/tools.py
+++ lib/tools.py
@@ -27,38 +27,35 @@ def binomial_tree_option_price(S, K, T, r, sigma, n, option_type='call'):
     return option_price
 
 def trinomial_tree_option_price(S, K, T, r, sigma, n, option_type='call'):
-    dt = T / n
-    u = np.exp(sigma * np.sqrt(2 * dt))
-    d = 1 / u
-    p = (np.exp(r * dt / 2) - np.exp(-sigma * np.sqrt(dt / 2))) / (np.exp(sigma * np.sqrt(dt / 2)) - np.exp(-sigma * np.sqrt(dt / 2)))
-    
-    # Constructing the trinomial tree
-    stock_price_tree = np.zeros((n + 1, 2 * n + 1))
-    stock_price_tree[0, n] = S
-    for i in range(1, n + 1):
-        for j in range(-i, i + 1):
-            if j == -i:
-                stock_price_tree[i, j + n] = stock_price_tree[i - 1, j + 1 + n] * u
-            elif j == i:
-                stock_price_tree[i, j + n] = stock_price_tree[i - 1, j - 1 + n] * d
-            else:
-                stock_price_tree[i, j + n] = stock_price_tree[i - 1, j + n] * np.sqrt(u)
-    
-    option_price_tree = np.zeros((n + 1, 2 * n + 1))
-    
-    # Calculate option prices at maturity
-    if option_type == 'call':
-        option_price_tree[n, :] = np.maximum(0, stock_price_tree[n, :] - K)
-    else:
-        option_price_tree[n, :] = np.maximum(0, K - stock_price_tree[n, :])
-    
-    # Backward induction to calculate option prices at earlier nodes
-    for i in range(n - 1, -1, -1):
-        for j in range(-i, i + 1):
-            option_price_tree[i, j + n] = np.exp(-r * dt) * (p * option_price_tree[i + 1, j + 1 + n] + (1 - p) * option_price_tree[i + 1, j + n] + p * option_price_tree[i + 1, j - 1 + n])
-    
-    return option_price_tree[0, n]
+    delta_t = T / n
+    u = np.exp(sigma * np.sqrt(2 * delta_t))
+    p_u = ((np.exp(r * delta_t/2) - np.exp(-sigma * np.sqrt(delta_t/2))) /
+           (np.exp(sigma * np.sqrt(delta_t/2)) - np.exp(-sigma * np.sqrt(delta_t/2))))**2
+    p_d = ((np.exp(sigma * np.sqrt(delta_t/2)) - np.exp(r * delta_t/2)) /
+           (np.exp(sigma * np.sqrt(delta_t/2)) - np.exp(-sigma * np.sqrt(delta_t/2))))**2
+    p_m = 1 - p_u - p_d
 
+    stock_prices = np.zeros((2*n+1, n+1))
+    option_values = np.zeros((2*n+1, n+1))
+    stock_prices[n,0] = S
+    for j in range(1,n+1):
+        stock_prices[n-j,j] = S*u**j
+        for i in range(n-j+1,n+j+1):
+            stock_prices[i,j] = stock_prices[i-1,j]/u
+    print(stock_prices)
+    if option_type == 'call':
+        option_values[:, -1] = np.maximum(0, stock_prices[:, -1] - K)
+    elif option_type == 'put':
+        option_values[:, -1] = np.maximum(0, K - stock_prices[:, -1])
+
+    for j in range(n-1, -1, -1):
+        for i in range(-j, j+1):
+            option_values[i+n, j] = np.exp(-r * delta_t) * (p_u * option_values[i+n+1, j+1] +
+                                                            p_d * option_values[i+n-1, j+1] +
+                                                            p_m * option_values[i+n, j+1])
+
+    option_price = option_values[n, 0]
+    return option_price
 
 def black_scholes_merton(S, K, T, r, sigma, option_type='call'):
     d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
